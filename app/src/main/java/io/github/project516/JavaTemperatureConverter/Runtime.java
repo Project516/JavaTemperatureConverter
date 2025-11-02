242cafa760845834b48945d7e192d67cbbad58aa
Project516 <138796702+Project516@users.noreply.github.com>
2025-11-02 19:52:56 +0000

    runtime

---
+++ app/src/main/java/io/github/project516/JavaTemperatureConverter/Runtime.java
@@ -0,0 +1,10 @@
+package io.github.project516.JavaTemperatureConverter;
+
+import io.github.project516.jlibutils.ScannerHelper;
+
+public class Runtime {
+
+    ScannerHelper scan = new ScannerHelper();
+
+    void run() {}
+}
